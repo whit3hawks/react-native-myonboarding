d94f9bb0bd8c89f47905804f1dff07825192f80a
Sharif Khaleel <ll.hawk.ll@gmail.com>
2022-01-31 02:16:15 +0500

    first commit

---
+++ dist/module/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Home = () => {
+  return /*#__PURE__*/React.createElement(View, null, "Hi");
+};
+
+export default Home;
+//# sourceMappingURL=index.js.map
